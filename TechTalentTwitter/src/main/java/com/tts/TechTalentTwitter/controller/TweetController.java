7198300c02b866a235935435f99bd4d00d43c847
Rachel DeCouto <55114977+redecouto@users.noreply.github.com>
2019-10-07 19:59:52 -0400

    the fourth push
    added two html pages

--- TechTalentTwitter/src/main/java/com/tts/TechTalentTwitter/controller/TweetController.java
+++ TechTalentTwitter/src/main/java/com/tts/TechTalentTwitter/controller/TweetController.java
@@ -1,7 +1,13 @@
+package com.tts.TechTalentTwitter.controller;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import com.tts.TechTalentTwitter.service.UserService;
+
+import com.tts.TechTalentTwitter.model.Tweet;
 import com.tts.TechTalentTwitter.service.TweetService;
 
 @Controller
@@ -11,4 +17,29 @@ public class TweetController {
 	
     @Autowired
     private TweetService tweetService;
+    
+    @GetMapping(value= {"/tweets", "/"})
+    public String getFeed(Model model){
+        List<Tweet> tweets = tweetService.findAll();
+        model.addAttribute("tweetList", tweets);
+        return "feed";
+    }
+    
+    @GetMapping(value = "/tweets/new")
+    public String getTweetForm(Model model) {
+        model.addAttribute("tweet", new Tweet());
+        return "newTweet";
+        @PostMapping(value = "/tweets")
+        public String submitTweetForm(@Valid Tweet tweet, BindingResult bindingResult, Model model) {
+            User user = userService.getLoggedInUser();
+            if (!bindingResult.hasErrors()) {
+                tweet.setUser(user);
+                tweetService.save(tweet);
+                model.addAttribute("successMessage", "Tweet successfully created!");
+                model.addAttribute("tweet", new Tweet());
+            }
+            return "newTweet";
+        
+    }
+}
 }
--- TechTalentTwitter/src/main/java/com/tts/TechTalentTwitter/service/TweetService.java
+++ TechTalentTwitter/src/main/java/com/tts/TechTalentTwitter/service/TweetService.java
@@ -1,3 +1,5 @@
+package com.tts.TechTalentTwitter.service;
+
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
